a2f8d700438a00a85547fdfafb76d24d79bdabfb
sherwinbenjamin <sherwinbenjamin2828@gmail.com>
2022-10-23 16:05:14 +0530

    urlfix

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter>
+  <BrowserRouter>
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
 
